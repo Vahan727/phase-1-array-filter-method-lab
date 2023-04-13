7731aeaf1c02e2b83c0a18adedc9845e21227140
Vahan Nadjarian <vahan@nadjarian.com>
2023-04-13 12:57:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching (array, driverName) {
+        return array.filter(array => array.toLowerCase() === driverName.toLowerCase()) 
+}
+console.log(findMatching(drivers, 'sally'))
+
+function fuzzyMatch(array, driverName) {
+    return array.filter(array => array.charAt(0) === driverName.charAt(0))
+}
+console.log(fuzzyMatch(drivers, 'Bane'))
+
+function matchName(array, driverName) {
+    return array.filter(array => array.name === driverName)
+}
+console.log(matchName(drivers, 'Sammy'))
